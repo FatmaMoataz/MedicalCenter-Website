4e377e46a9f4e32f5a856d665f02be37a203725b
FatmaMoataz <fatmamoataz65@gmail.com>
2025-05-17 17:28:07 +0200

    Updated server.js

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-import jsonServer from 'json-server';
-
-const PORT = process.env.PORT || 8080;
+const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
-const router = jsonServer.router('db/data.json');  
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+server.use(cors());
 server.use(middlewares);
-server.use(router);
+server.use('/api', router);
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+module.exports = server;
